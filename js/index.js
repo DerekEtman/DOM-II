116dab21544bbb18b2ab7f7ee98fc22e79c65261
Derek Etman <dereketman@gmail.com>
2019-08-06 15:47:39 -0700

    transformed some images

--- js/index.js
+++ js/index.js
@@ -1,1 +1,48 @@
 // Your code goes here
+
+//random color gen
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+/* Nav &  header*/
+
+  // changes font color when mouse moves over logo.
+const logoMouse = document.querySelector('.logo-heading');
+logoMouse.addEventListener('mouseover', () => {
+    logoMouse.style.color = getRandomColor();
+})
+
+const navBg = document.querySelector('.main-navigation');
+navBg.addEventListener('mouseover', (e) => {
+  navBg.style.backgroundColor = getRandomColor();
+})
+
+const  busImg = document.querySelector('.intro img');
+
+busImg.addEventListener('dblclick', () => {
+  // console.log('Hello!')
+  busImg.style.transform = 'rotate(180deg)';
+})
+
+
+
+
+// /*  Main content */
+
+// const letsGo = document.querySelector('.content-section .text-content').style.resize =
+// "both";
+
+
+
+
+const busCrash = document.querySelector('.busCrash img')
+
+busCrash.addEventListener('wheel', () => {
+    console.log('Hello! BusCrash')
+})
